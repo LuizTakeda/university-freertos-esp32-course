704fd403ba04fbc323cef52d408d6df0539b72c0
LuizTakeda <luizgustavoglt@gmail.com>
2025-08-06 20:46:14 -0300

    feat(dashboard): close menu when user clicks an option

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -5,8 +5,10 @@ import ThemeChangerElement from "./components/theme-changer/theme-changer";
 import DashboardStruct from "./components/dashboard/dashboard";
 import IconElement from "./components/icon/icon";
 import ImageElement from "./components/img/img";
+import FormElement from "./components/form/form";
 
 ImageElement;
 IconElement;
 ThemeChangerElement;
 DashboardStruct;
+FormElement;
