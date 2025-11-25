2555416976bbcbd915390053c116c95b54ef2a55
LuizTakeda <luizgustavoglt@gmail.com>
2025-11-24 22:25:23 -0300

    feat(analog_input): add basic structure

---
+++ components/analog_input/analog_input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "analog_input.h"
+
+esp_err_t analog_input_initialize(void)
+{
+  return ESP_OK;
+}
+
+esp_err_t analog_input_add_event_handler(analog_input_event_handler_t handler)
+{
+  return ESP_OK;
+}
