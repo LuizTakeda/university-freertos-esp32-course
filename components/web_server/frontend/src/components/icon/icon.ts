add7e2bca223dce9951b04c5b5686ba5dbdd8cc7
LuizTakeda <luizgustavoglt@gmail.com>
2026-01-27 21:38:15 -0300

    refactor(front-end): improve code

--- components/web_server/frontend/src/components/icon/icon.ts
+++ components/web_server/frontend/src/components/icon/icon.ts
@@ -1,42 +1,47 @@
 import "./icon.scss";
 import ArrowUp from "./svg/alt-arrow-up-svgrepo-com.svg";
 import SortHorizontal from "./svg/square-sort-horizontal-svgrepo-com.svg";
-import Tunning from "./svg/tuning-square-2-svgrepo-com.svg"
+import Tunning from "./svg/tuning-square-2-svgrepo-com.svg";
 
-type Name = "" | "moon" | "sun" | "arrow-up" | "home" | "cpu" | "settings" | "sort-horizontal" | "tunning";
+type Name = "arrow-up" | "sort-horizontal" | "tunning";
+
+const ICON_MAP: Record<Name, string> = {
+  "arrow-up": ArrowUp,
+  "sort-horizontal": SortHorizontal,
+  "tunning": Tunning,
+};
 
 export default class IconElement extends HTMLElement {
-  private _name: Name;
+  static get observedAttributes() {
+    return ["name"];
+  }
 
   constructor() {
     super();
-
-    this._name = (this.getAttribute("name") ?? "") as Name;
   }
 
-  connectedCallback() {
-    switch (this._name) {
-      case "":
-        break;
-
-      case "arrow-up":
-        this.innerHTML = ArrowUp;
-        break
-
-      case "sort-horizontal":
-        this.innerHTML = SortHorizontal;
-        break
-
-      case "tunning":
-        this.innerHTML = Tunning;
-        break
-
-      default:
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (name === "name" && oldValue !== newValue) {
+      this._render();
     }
   }
 
-  set name(value: Name) { this._name = value }
-  get name(): Name { return this._name ?? "" }
+  connectedCallback() {
+    this._render();
+  }
+
+  private _render() {
+    const name = this.getAttribute("name") as Name;
+    this.innerHTML = ICON_MAP[name] || "";
+  }
+
+  get name(): Name {
+    return this.getAttribute("name") as Name;
+  }
+
+  set name(value: Name) {
+    this.setAttribute("name", value);
+  }
 }
 
 customElements.define("icon-element", IconElement);
